7ea95d706e43f439de6fcf72a3c4d5c8dfef7da0
besmir miftari <besmirmiftari5@gmail.com>
2023-03-21 19:53:27 +0100

    Add extra content

---
+++ TestApplication/TestApplication/Controller/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TestApplication.Controller
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult TestPage()
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
